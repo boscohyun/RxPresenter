ff139b2de902695e0ca34551bc1f0256c49632a6
boscohyun <boscohyun@gmail.com>
2021-05-21 15:48:42 +0900

    Add unit tests

---
+++ Assets/Plugins/RxPresenter/Tests/EditMode/ViewAnimatorTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace Boscohyun.RxPresenter.Tests.EditMode
+{
+    using Fixtures;
+    
+    public class ViewAnimatorTest
+    {
+        [TestCase(false)]
+        [TestCase(true)]
+        public void Construct(bool animatorAlwaysActive)
+        {
+            var viewAnimator = new ViewAnimator(animatorAlwaysActive);
+            Assert.AreEqual(animatorAlwaysActive, viewAnimator.AnimatorAlwaysActive);
+        }
+        
+        [Test, Combinatorial]
+        public void SetActive(
+            [Values(false, true)] bool animatorAlwaysActive,
+            [Values(false, true)] bool active)
+        {
+            var viewAnimator = new ViewAnimator(animatorAlwaysActive);
+            viewAnimator.SetActive(active);
+            Assert.AreEqual(active, viewAnimator.ActiveSelf);
+        }
+
+        [Test, Combinatorial]
+        public void PlayAnimation(
+            [Values(false, true)] bool animatorAlwaysActive,
+            [Values(ViewAnimatorState.Hide, ViewAnimatorState.Show)]
+            ViewAnimatorState viewAnimatorState,
+            [Range(0f, 1f, 0.1f)] float normalizedTime)
+        {
+            var viewAnimator = new ViewAnimator(animatorAlwaysActive);
+            viewAnimator.PlayAnimation(viewAnimatorState, normalizedTime);
+            Assert.AreEqual(viewAnimatorState, viewAnimator.CurrentAnimatorState);
+            Assert.AreEqual(normalizedTime, viewAnimator.CurrentAnimatorStateNormalizedTime);
+        }
+    }
+}
---
+++ Assets/Plugins/RxPresenter/Tests/EditMode/ViewTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Boscohyun.RxPresenter.Tests.EditMode
+{
+    using Fixtures;
+    
+    public class ViewTest
+    {
+        [TestCase(false)]
+        [TestCase(true)]
+        public void Construct(bool active)
+        {
+            var view = new View(active);
+            Assert.AreEqual(active, view.ActiveSelf);
+            Assert.False(view.HasViewAnimator);
+            Assert.Null(view.ViewAnimator);
+            
+            var animatedView = new View(active, new ViewAnimator());
+            Assert.AreEqual(active, animatedView.ActiveSelf);
+            Assert.True(animatedView.HasViewAnimator);
+            Assert.NotNull(animatedView.ViewAnimator);
+        }
+
+        [TestCase(false)]
+        public void SetActive(bool active)
+        {
+            var view = new View(false);
+            view.SetActive(active);
+            Assert.AreEqual(active, view.ActiveSelf);
+            view.SetActive(!active);
+            Assert.AreEqual(!active, view.ActiveSelf);
+        }
+    }
+}
