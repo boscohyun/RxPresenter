74a0a097bf38df254f0d3f70b5be5c9f3d8e4dcb
boscohyun <boscohyun@gmail.com>
2021-06-19 18:54:25 +0900

    Update examples already exist

---
+++ Assets/Plugins/RxPresenter-Examples/00-Common/Editor/TextButtonEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+namespace Boscohyun.RxPresenter.Examples.Editor
+{
+    [CustomEditor(typeof(TextButton))]
+    public class TextButtonEditor : ButtonEditor
+    {
+        private SerializedProperty _text;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            _text = serializedObject.FindProperty("text");
+        }
+        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            EditorGUILayout.PropertyField(_text);
+        }
+    }
+}
