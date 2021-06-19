381f592cc548c6fed618186af72357233229eaf0
boscohyun <boscohyun@gmail.com>
2021-06-19 18:55:52 +0900

    Introduce external DOTween support scripts and asmdef

---
+++ Assets/Plugins/RxPresenter/External/DOTween/Scripts/DOTweenReactivePresenter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UniRx;
+
+namespace Boscohyun.RxPresenter.External.DOTween
+{
+    public abstract class DOTweenReactivePresenter<T> : DOTweenPresenter, IReactivePresenter<T>
+        where T : DOTweenReactivePresenter<T>
+    {
+        public IObservable<T> OnPresenterStateChange => Humble.OnPresenterStateChange.Select(_ => (T) this);
+        public IObservable<T> OnShowAnimationBeginning => Humble.OnPresenterStateChange.Select(_ => (T) this);
+        public IObservable<T> OnShowAnimationEnd => Humble.OnPresenterStateChange.Select(_ => (T) this);
+        public IObservable<T> OnHideAnimationBeginning => Humble.OnPresenterStateChange.Select(_ => (T) this);
+        public IObservable<T> OnHideAnimationEnd => Humble.OnPresenterStateChange.Select(_ => (T) this);
+
+        public void Show(Action<T> callback) => Humble.Show(_ => callback?.Invoke((T) this));
+
+        public void Show(bool skipAnimation, Action<T> callback) =>
+            Humble.Show(skipAnimation, _ => callback?.Invoke((T) this));
+
+        public IObservable<T> ShowAsObservable(bool skipAnimation = default) =>
+            Humble.ShowAsObservable(skipAnimation).Select(_ => (T) this);
+
+        public void Hide(Action<T> callback) => Humble.Hide(_ => callback?.Invoke((T) this));
+
+        public void Hide(bool skipAnimation, Action<T> callback) =>
+            Humble.Hide(skipAnimation, _ => callback?.Invoke((T) this));
+
+        public IObservable<T> HideAsObservable(bool skipAnimation = default) =>
+            Humble.HideAsObservable(skipAnimation).Select(_ => (T) this);
+    }
+}
---
+++ Assets/Plugins/RxPresenter/External/DOTween/Scripts/DOTweenViewAnimator.cs
@@ -0,0 +1,75 @@
+﻿using DG.Tweening;
+
+namespace Boscohyun.RxPresenter.External.DOTween
+{
+    public class DOTweenViewAnimator : IViewAnimator
+    {
+        private readonly Tween _showTween;
+        private readonly Tween _hideTween;
+        private ViewAnimatorState _lastPlayedState;
+
+        public readonly bool hasTween;
+
+        public DOTweenViewAnimator(Tween showTween, Tween hideTween)
+        {
+            _showTween = showTween;
+            _hideTween = hideTween;
+            hasTween = !(_showTween is null) ||
+                       !(_hideTween is null);
+        }
+
+        #region IViewAnimator
+
+        int IViewAnimator.AnimatorActiveDelayFrame => 0;
+
+        bool IViewAnimator.AnimatorAlwaysActive => true;
+
+        ViewAnimatorState IViewAnimator.CurrentAnimatorState => _lastPlayedState;
+
+        float IViewAnimator.CurrentAnimatorStateNormalizedTime
+        {
+            get
+            {
+                var currentTween = ((IViewAnimator) this).CurrentAnimatorState == ViewAnimatorState.Show
+                    ? _showTween
+                    : _hideTween;
+
+                return currentTween.ElapsedPercentage();
+            }
+        }
+
+        void IViewAnimator.PlayAnimation(ViewAnimatorState viewAnimatorState, float normalizedTime)
+        {
+            _lastPlayedState = viewAnimatorState;
+
+            if (viewAnimatorState == ViewAnimatorState.Show)
+            {
+                if (_showTween is null)
+                {
+                    return;
+                }
+
+                var time = _showTween.Duration() * normalizedTime;
+                _showTween.Goto(time, true);
+            }
+            else
+            {
+                if (_hideTween is null)
+                {
+                    return;
+                }
+
+                var time = _hideTween.Duration() * normalizedTime;
+                _hideTween.Goto(time, true);
+            }
+        }
+
+        void IViewAnimator.SetActive(bool active)
+        {
+            // NOTE: Do nothing.
+            // bool IViewAnimator.AnimatorAlwaysActive => true;
+        }
+
+        #endregion
+    }
+}
